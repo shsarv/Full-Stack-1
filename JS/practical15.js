e175ca30450bf4e55bbc14f9443fdff9a22a29b6
Sarvesh Kumar Sharma <shsarv@gmail.com>
2020-10-30 13:34:49 +0530

    Added Js File

--- JS/practical15.js
+++ JS/practical15.js
@@ -1,21 +1,51 @@
-var button = document.getElementById("increment"),
-    count = 0;
-button.onclick = function() {
+var count = 0;
+
+// disabling decrement button for count==0.
+
+document.getElementById("decrement").disabled = true;
+document.getElementById("decrement").style.backgroundColor = "#99003d";
+document.getElementById("decrement").style.boxShadow = " 0 9px #260d0d";
+
+// increment function
+
+function increment() {
+
+    //Activating decrement key for count>0.
+
+    document.getElementById("decrement").disabled = false;
+    document.getElementById("decrement").style.backgroundColor = " #ff0066";
+    document.getElementById("decrement").style.boxShadow = " 0 9px #999";
+
+    //Increasing count value
+
     count += 1;
-    button.innerHTML = count;
-};
-var button = document.getElementById("decrement"),
-    count = 0;
-button.onclick = function() {
-    if (count > 0) {
-        document.getElementById("decrement").disabled = false;
+    document.getElementById("counter").innerHTML = count;
+}
+
+//decrement Function
+
+function decrement() {
+
+    //check if count==0 then disable decrement button if not decrease the count value.
+
+    if (count == 0) {
+        document.getElementById("decrement").disabled = true;
+        document.getElementById("decrement").style.backgroundColor = "#99003d";
+        document.getElementById("decrement").style.boxShadow = " 0 9px #260d0d";
+
+    } else {
         count -= 1;
-        button.innerHTML = count;
+        document.getElementById("counter").innerHTML = count;
+        document.getElementById("decrement").style.backgroundColor = " #ff0066";
+        document.getElementById("decrement").style.boxShadow = " 0 9px #999";
     }
-};
-var button = document.getElementById("counter"),
-    count = 0;
-button.onclick = function() {
-    // count -= 1;
-    button.innerHTML = count;
-};
+
+    // for last time when count value goes to zero after decrementing, disabling the decrement key
+
+    if (count == 0) {
+        document.getElementById("decrement").disabled = true;
+        document.getElementById("decrement").style.backgroundColor = "#99003d";
+        document.getElementById("decrement").style.boxShadow = " 0 9px #260d0d";
+
+    }
+}
